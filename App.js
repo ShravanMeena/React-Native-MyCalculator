5cab4192287ad45cb8b2fac927c303b7602c121e
Shravan <41790742+ShravanMeena@users.noreply.github.com>
2020-03-01 00:55:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+// import Calculator from './components/Calculator'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Yolo
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Constants.statusBarHeight,
+  },
+})
